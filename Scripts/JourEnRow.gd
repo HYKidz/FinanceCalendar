c7e0001f1f7a7eb79e606f8d9b3569f0abc1a1e7
Samahel Brassard <HYKinteractive@hotmail.com>
2024-01-30 15:24:13 -0500

    Going to test sumthang

---
+++ Scripts/JourEnRow.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+var jour = ["Dimanche","Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi"]
+var jour_node_array =[]
+
+@onready var container = get_node("Box/Container/Grid")
+@onready var jour_node = preload("res://Node/jour.tscn")
+# Called when the node enters the scene tree for the first time.
+
+func _ready():
+	jour_node_array.resize(7)
+	for d in jour:
+		var new_jour = jour_node.instantiate()
+		new_jour.name = d
+		jour_node_array.fill(new_jour)
+		container.add_child(new_jour)
+	
+	for i in 7:
+		print(i)
+		var jour_en_cour = jour_node_array[i]
+		jour_en_cour.set_Jour = jour[i]
+
--- Scripts/calendar.gd
+++ Scripts/calendar.gd
@@ -1,9 +1,16 @@
 extends Node2D
 
-var jour = ["Dimanche","Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi"]
+
 var mois = ["Janvier","Fevrier","Mars","Avril","Mai","Juin","Juillet","Aout","Septembre","Octobre","Novembre","Decembre"]
+
+
+# @onready var canvasgroup = get_node("BoxContainer")
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	
+		# new_jour.set_Jour = str(d)
+		# canvasgroup.add_child(new_jour)
+
 	pass # Replace with function body.
